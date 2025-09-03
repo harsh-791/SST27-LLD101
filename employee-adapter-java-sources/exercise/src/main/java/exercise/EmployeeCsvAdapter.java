d325b3b0e6716d7d57e741fee14e1fe9787f0bf2
Harsh <harsh.verma1503@gmail.com>
2025-09-03 16:09:09 +0530

    ding ding

---
+++ employee-adapter-java-sources/exercise/src/main/java/exercise/EmployeeCsvAdapter.java
@@ -0,0 +1,28 @@
+public class EmployeeCsvAdapter implements Employee {
+    private final EmployeeCSV employeeCSV;
+
+    public EmployeeCsvAdapter(EmployeeCSV employeeCSV) {
+        this.employeeCSV = employeeCSV;
+    }
+
+    @Override
+    public String getId() {
+        return employeeCSV.tokens()[0];
+    }
+
+    @Override
+    public String getFirstName() {
+        return employeeCSV.tokens()[1];
+    }
+
+    @Override
+    public String getLastName() {
+        return employeeCSV.tokens()[2];
+    }
+
+    @Override
+    public String getEmail() {
+        return employeeCSV.tokens()[3];
+    }
+
+}
