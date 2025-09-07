6e642af67956f6325aa213f8504ba4dfbb4656d9
Harsh <harsh.verma1503@gmail.com>
2025-09-07 23:32:18 +0530

    Glyph

--- design-pattern-assignments/flyweight-glyphs/src/com/example/render/App.java
+++ design-pattern-assignments/flyweight-glyphs/src/com/example/render/App.java
@@ -4,13 +4,5 @@ public class App {
     public static void main(String[] args) {
         Renderer r = new Renderer();
         System.out.println("Cost=" + r.render("Hello Flyweight! ".repeat(2000)));
-
-        // Test reference equality for same style configs
-        TextStyle style1 = TextStyleFactory.getTextStyle("Inter", 14, true);
-        TextStyle style2 = TextStyleFactory.getTextStyle("Inter", 14, true);
-        System.out.println("Same style reference equality: " + (style1 == style2));
-
-        TextStyle style3 = TextStyleFactory.getTextStyle("Inter", 14, false);
-        System.out.println("Different style reference equality: " + (style1 == style3));
     }
 }
